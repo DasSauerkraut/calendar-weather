5add2e5395570acbc825c1ede69bd1dc649480a1
DasSauerkraut <cspudsy@yahoo.com>
2020-02-06 15:01:09 -0600

    implemented time display

--- calendar.js
+++ calendar.js
@@ -26,18 +26,45 @@ class Calendar extends Application {
     templateData.dt.addWeekday("Wednesday");
     templateData.dt.addWeekday("Thursday");
     templateData.dt.setYear(1234);
-    templateData.dt.setEra("IC")
+    templateData.dt.setEra("IC");
+    templateData.dt.setWeekday("Monday")
+    templateData.dt.setTimeDisp();
+    templateData.dt.setDayLength(24);
     templateData.dt.genDateWordy();
+  }
+
+  updateDisplay(){
+    document.getElementById("calendar-text").innerHTML = templateData.dt.dateWordy;
+    document.getElementById("calendar-weekday").innerHTML = templateData.dt.currentWeekday;
+    document.getElementById("calendar-time").innerHTML = templateData.dt.timeDisp;
   }
 
   activateListeners(html){
     const nextDay = '#calendar-btn-day';
+    const quickAction = '#calendar-btn-quick';
+    const longAction = '#calendar-btn-long';
+    const nightSkip = '#calendar-btn-night';
+    this.updateDisplay()
     html.find(nextDay).click(ev => {
       ev.preventDefault();
-      templateData.dt.advanceDay();
-      document.getElementById("calendar-text").innerHTML = templateData.dt.dateWordy;
-      document.getElementById("calendar-weekday").innerHTML = templateData.dt.currentWeekday;
-    })
+      templateData.dt.advanceMorning();
+      this.updateDisplay()
+    });
+    html.find(quickAction).click(ev => {
+      ev.preventDefault();
+      templateData.dt.quickAction();
+      this.updateDisplay();
+    });
+    html.find(longAction).click(ev => {
+      ev.preventDefault();
+      templateData.dt.advanceHour();
+      this.updateDisplay();
+    });
+    html.find(nightSkip).click(ev => {
+      ev.preventDefault();
+      templateData.dt.advanceNight();
+      this.updateDisplay();
+    });
   }
 }
 class Month {
@@ -62,11 +89,61 @@ class DateTime {
   currentWeekday = "";
   dateWordy ="";
   era = "";
+  minutes = 0;
+  hours = 0;
+  dayLength = 0;
+  timeDisp = "";
 
   addMonth(month){this.months.push(month)};
   addWeekday(day){this.daysOfTheWeek.push(day)};
   setYear(year){this.year = year}
   setEra(era){this.era = era}
+  setDayLength(length) {this.dayLength = length}
+  setWeekday(day){this.currentWeekday = day}
+
+  setTimeDisp(){
+    let minDisp = ""
+    if(this.minutes == 0){
+      minDisp = "00";
+    } else {
+      minDisp = this.minutes * 15;
+    }
+    this.timeDisp = this.hours + ":" + minDisp;
+  }
+
+  quickAction(){
+    if(this.minutes == 3){
+      this.advanceHour()
+      this.minutes = 0;
+    } else {
+      this.minutes += 1;
+    }
+    this.setTimeDisp();
+  }
+
+  advanceHour(){
+    if(this.hours == this.dayLength - 1){
+      this.advanceDay();
+      this.hours = 0;
+    } else {
+      this.hours += 1;
+    }
+    this.setTimeDisp();
+  }
+
+  advanceNight(){
+    this.advanceDay();
+    this.hours = 0;
+    this.minutes = 0;
+    this.setTimeDisp();
+  }
+
+  advanceMorning(){
+    this.advanceDay();
+    this.hours = 7;
+    this.minutes = 0;
+    this.setTimeDisp();
+  }
 
   genDateWordy(){
     let dayAppendage = "";

25c04a96ababc7ba4f42abe07faf8daf1958b672
DasSauerkraut <cspudsy@yahoo.com>
2020-02-06 14:39:06 -0600

    Functioning day/month/year rotation

--- calendar.js
+++ calendar.js
@@ -13,20 +13,30 @@ class Calendar extends Application {
   getData() {
     return templateData;
   }
+
+  populateData(){
+    let newMonth1 = new Month("Month 1", 1, true);
+    templateData.dt.addMonth(newMonth1);
+    let newMonth2 = new Month("Month 2", 2, true);
+    templateData.dt.addMonth(newMonth2);
+    let newMonth3 = new Month("Wow look at this incredibly long month name", 11, true);
+    templateData.dt.addMonth(newMonth3);
+    templateData.dt.addWeekday("Monday");
+    templateData.dt.addWeekday("Tuesday");
+    templateData.dt.addWeekday("Wednesday");
+    templateData.dt.addWeekday("Thursday");
+    templateData.dt.setYear(1234);
+    templateData.dt.setEra("IC")
+    templateData.dt.genDateWordy();
+  }
+
   activateListeners(html){
-    const nextDay = '#calendar-btn-right';
-    const prevDay = '#calendar-btn-left'
+    const nextDay = '#calendar-btn-day';
     html.find(nextDay).click(ev => {
       ev.preventDefault();
-      templateData.day += 1;
-      var output = templateData.day + "-" + templateData.month + "-" + templateData.year;
-      document.getElementById("calendar-text").innerHTML = output;
-    })
-    html.find(prevDay).click(ev => {
-      ev.preventDefault();
-      templateData.day -= 1;
-      var output = templateData.day + "-" + templateData.month + "-" + templateData.year;
-      document.getElementById("calendar-text").innerHTML = output;
+      templateData.dt.advanceDay();
+      document.getElementById("calendar-text").innerHTML = templateData.dt.dateWordy;
+      document.getElementById("calendar-weekday").innerHTML = templateData.dt.currentWeekday;
     })
   }
 }
@@ -34,6 +44,7 @@ class Month {
   name = "";
   length = 0;
   isNumbered = true;
+  abbrev = "";
   constructor(name, length, isNumbered){
     this.name = name;
     this.length = length;
@@ -48,31 +59,59 @@ class DateTime {
   day = 0;
   numDayOfTheWeek = 0;
   currentMonth = 0;
-  addMonth(month){this.months.append(month)};
+  currentWeekday = "";
+  dateWordy ="";
+  era = "";
+
+  addMonth(month){this.months.push(month)};
+  addWeekday(day){this.daysOfTheWeek.push(day)};
+  setYear(year){this.year = year}
+  setEra(era){this.era = era}
+
+  genDateWordy(){
+    let dayAppendage = "";
+    if(this.day % 10 == 1 && this.day != 11){
+      dayAppendage = "st";
+    } else if (this.day % 10 == 2) {
+      dayAppendage = "nd";
+    } else if (this.day % 10 == 3) {
+      dayAppendage = "rd";
+    } else {
+      dayAppendage = "th";
+    }
+    this.dateWordy = this.day + dayAppendage + " of " 
+      + this.months[this.currentMonth].name + ", " + this.year + " " + this.era;
+  }
 
   advanceDay(){
-    if (day == this.months[this.currentMonth].length){
-      day = 1;
+    if (this.day == this.months[this.currentMonth].length){
+      this.day = 1;
       this.advanceMonth();
       if(this.daysOfTheWeek[this.numDayOfTheWeek+1] == null){
         this.numDayOfTheWeek = 0;
+        this.currentWeekday = this.daysOfTheWeek[this.numDayOfTheWeek];
       } else {
         this.numDayOfTheWeek += 1;
+        this.currentWeekday = this.daysOfTheWeek[this.numDayOfTheWeek];
       }
     } else {
-      day += 1;
+      this.day += 1;
       if(this.daysOfTheWeek[this.numDayOfTheWeek+1] == null){
         this.numDayOfTheWeek = 0;
+        this.currentWeekday = this.daysOfTheWeek[this.numDayOfTheWeek];
       } else {
         this.numDayOfTheWeek += 1;
+        this.currentWeekday = this.daysOfTheWeek[this.numDayOfTheWeek];
       }
     }
+    this.genDateWordy()
   }
 
   advanceMonth(){
-    if(this.month[this.currentMonth+1] == null){
+    let lookforward = this.currentMonth+1;
+    if(this.months[lookforward] == null){
       this.currentMonth = 0;
-      year += 1;
+      this.year += 1;
     } else {
       this.currentMonth += 1;
     }
@@ -84,12 +123,11 @@ $(document).ready(() => {
   
   
   templateData = {
-    month: 0,
-    day: 0,
-    year: 0,
-    date: "new",
+    dt: new DateTime()
   }
+
   let c = new Calendar();
+  c.populateData();
   // Settings
   Hooks.on('init', ()=> {
     game.settings.register('calendar-weather', 'Calendar Config', {
@@ -107,4 +145,6 @@ $(document).ready(() => {
       c.render(true);
     });
   });
+
+  
 });
